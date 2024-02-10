9a21360381d04b7fde4cac230a213ca0ef105985
Vayom <korefan639@gmail.com>
2024-02-10 06:26:02 +0300

    Create decorators for teachers and students url

---
+++ teaching/utils.py
@@ -0,0 +1,34 @@
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+
+from teaching.models import Student, Teacher
+
+
+def student_required(view_func):
+    def wrapper(request, *args, **kwargs):
+        user = request.user
+        try:
+            student = Student.objects.get(user=user)
+        except Student.DoesNotExist:
+            student = None
+        if student:
+            return view_func(request, *args, **kwargs)
+        else:
+            return redirect(reverse_lazy('home'))
+
+    return wrapper
+
+
+def teacher_required(view_func):
+    def wrapper(request, *args, **kwargs):
+        user = request.user
+        try:
+            student = Teacher.objects.get(user=user)
+        except Teacher.DoesNotExist:
+            student = None
+        if student:
+            return view_func(request, *args, **kwargs)
+        else:
+            return redirect(reverse_lazy('home'))
+
+    return wrapper
